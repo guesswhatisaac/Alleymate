b0f85610aef9fe209f9be80814eaa17202d04038
guesswhatisaac <guesswhatisaac@gmail.com>
2025-06-12 18:24:15 +0800

    catalogue screen re-added. was not previously committed.

---
+++ app/src/main/java/com/mobdeve/s18/roman/isaacnathan/alleymate/ui/catalogue/CatalogueScreen.kt
@@ -0,0 +1,19 @@
+package com.mobdeve.s18.roman.isaacnathan.alleymate.ui.catalogue
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+// TODO: implement Catalogue Screen
+@Composable
+fun CatalogueScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Catalogue Screen")
+    }
+}
